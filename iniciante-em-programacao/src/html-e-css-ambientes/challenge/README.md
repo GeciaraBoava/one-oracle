8a3c5c2261deb6197a82c3f99bcbaa5ec5dc1ed2
GeciaraBoava <geciaracardoso@gmail.com>
2024-07-08 13:15:31 -0300

    Conclusão aulas primeira fase e challenge

---
+++ iniciante-em-programacao/src/html-e-css-ambientes/challenge/README.md
@@ -0,0 +1,9 @@
+# Decodificador
+Encriptador de palavras desenvolvido como desafio na Formação em Programação Básica do One Oracle Next Education.
+
+## Linguagens e recursos
+![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+![HTML](https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white)
+![CSS](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+
---
+++ iniciante-em-programacao/src/html-e-css-ambientes/challenge/scripts/script.js
@@ -0,0 +1,49 @@
+
+function criptografar() {
+    var entradaTexto = document.getElementById("entradaTexto").value;
+    var saidaTexto = document.getElementById("saidaTexto");
+    var saidaOrientacoes = document.getElementById("saidaOrientacoes");
+    var saidaResultado = document.getElementById("saidaResultado");
+
+    var criptografado = entradaTexto.replace(/[aeiou]/g, function(substituir) {
+        switch(substituir) {
+            case "a": return "ai";
+            case "e": return "enter";
+            case "i": return "imes";
+            case "o": return "ober";
+            case "u": return "ufat";
+        }
+    });
+    saidaTexto.innerText = criptografado;
+    saidaOrientacoes.style.display = "none";
+    saidaResultado.style.display= "block";
+}
+
+function descriptografar() {
+    var entradaTexto = document.getElementById("entradaTexto").value;
+    var saidaTexto = document.getElementById("saidaTexto");
+    var saidaOrientacoes = document.getElementById("saidaOrientacoes");
+    var saidaResultado = document.getElementById("saidaResultado");
+
+    var descriptografado = entradaTexto.replace(/ai|enter|imes|ober|ufat/g, function(substituir) {
+        switch(substituir) {
+            case "ai": return "a";
+            case "enter": return "e";
+            case "imes": return "i";
+            case "ober": return "o";
+            case "ufat": return "u";
+        }
+    });
+    saidaTexto.innerText = descriptografado;
+    saidaOrientacoes.style.display = "none";
+    saidaResultado.style.display = "block";
+}
+
+function copiar() {
+    var saidaTexto = document.getElementById("saidaTexto").innerText;
+    navigator.clipboard.writeText(saidaTexto).then(function() {
+        alert("Texto copiado para a área de transferência!");
+    }, function(err) {
+        alert("Erro ao copiar o texto: " + err);
+    });
+}
